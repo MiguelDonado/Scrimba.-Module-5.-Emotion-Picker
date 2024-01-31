27da0813f2b03e2dd8b8a68bc7eb7bbc0db89ad3
MiguelDonado <miguel.5799@hotmail.es>
2024-01-31 17:16:10 +0100

    Finish header styling and structure of header element

---
+++ data.js
@@ -0,0 +1,64 @@
+const gogginsData = [
+    {
+        emotionTags:["focused"],
+        isGif:false,
+        image:"focused.jpg",
+        alt:"David Goggins focused",
+    },
+    {
+        emotionTags:["motivated"],
+        isGif:false,
+        image:"motivated.jpg",
+        alt:"David Goggins motivated",
+    },
+    {
+        emotionTags:["sincered","psychoanalyst"],
+        isGif:false,
+        image:"sincered.jpg",
+        alt:"David Goggins sincered",
+    },
+
+    {
+        emotionTags:["psychoanalyst"],
+        isGif:false,
+        image:"psychoanalyst.jpg",
+        alt:"David Goggins psychoanalyst mode",
+    },
+    {
+        emotionTags:["euphoric","motivated"],
+        isGif:false,
+        image:"euphoric.jpg",
+        alt:"David Goggins euphoric",
+    },
+    {
+        emotionTags:["focused"],
+        isGif:true,
+        image:"focused.gif",
+        alt:"David Goggins focused",
+    },
+    {
+        emotionTags:["euphoric","motivated"],
+        isGif:true,
+        image:"euphoric.gif",
+        alt:"David Goggins euphoric",
+    },
+    {
+        emotionTags:["motivated"],
+        isGif:true,
+        image:"motivated.gif",
+        alt:"David Goggins motivated while training",
+    },
+    {
+        emotionTags:["sincered","psychoanalyst"],
+        isGif:true,
+        image:"sincered.gif",
+        alt:"David Goggins being sincere about running",
+    },
+    {
+        emotionTags:["psychoanalyst"],
+        isGif:true,
+        image:"psychoanalyst.gif",
+        alt:"David Goggins psychoanalist mode",
+    },
+
+]
